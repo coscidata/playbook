6e6b20e063434a662aa4492e5000eb637deb4f8e
Peter He <hjc1702@gmail.com>
2017-09-21 17:14:47 +0800

    centos7 的 yum 用的 Python 2 写的，所以不能链接成 python

--- centos_install_python3.sh
+++ centos_install_python3.sh
@@ -7,5 +7,5 @@ yum -y install https://centos7.iuscommunity.org/ius-release.rpm
 yum -y install python36u
 yum -y install python36u-pip
 
-ln -s -f /usr/bin/python3.6 /usr/bin/python
+ln -s -f /usr/bin/python3.6 /usr/bin/python3
 ln -s -f /usr/bin/pip3.6 /usr/bin/pip
