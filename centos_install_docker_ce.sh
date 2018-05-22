0e2b34ec01794dea4a59a80f4c46aa52a5644c1f
Peter <hjc1702@gmail.com>
2018-05-22 11:42:25 +0800

    add install docker sh

---
+++ centos_install_docker_ce.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Uninstall old versions
+yum remove docker \
+           docker-client \
+           docker-client-latest \
+           docker-common \
+           docker-latest \
+           docker-latest-logrotate \
+           docker-logrotate \
+           docker-selinux \
+           docker-engine-selinux \
+           docker-engine
+
+# SET UP THE REPOSITORY
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+# INSTALL DOCKER CE
+yum install -y docker-ce
+
+# start and run
+systemctl start docker
+docker run hello-world
